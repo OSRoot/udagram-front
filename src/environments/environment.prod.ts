7824e6139f9e9aac828ac1f61f3bc358a4622ef8
OSRoot <osamaessayed.el.4468@azhar.edu.eg>
2023-02-05 21:30:55 -0500

    my sec commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://udagramapi-env-1.eba-5xmpkp7c.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
